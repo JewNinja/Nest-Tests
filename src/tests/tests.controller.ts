4d9bb635843193b3c2fa97640fbca3a7a6e7f1ed
Stepan Voroviov <stepan0168@gmail.com>
2021-02-20 17:52:05 +0300

    First endpoint

---
+++ src/tests/tests.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('tests')
+export class TestsController {
+  @Get('')
+  findAll(): any {
+    console.log('453');
+    return { data: 'This action returns all cats'};
+    // return 'This action returns all cats';
+  }
+}
